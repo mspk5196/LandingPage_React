839ddce300af902b482be5ea045a23913ec6c66a
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-02-10 19:53:39 +0530

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/Home'>
+  <BrowserRouter>
   <StrictMode>
     <App/>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/LandingPage_React"
+  base:"/LandingPage_React/"
 })

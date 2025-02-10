17a1f2a346499463631c1d387b6ec67fbfb1c606
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-02-10 19:50:11 +0530

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/LandingPage_React'>
+  <BrowserRouter basename='/Home'>
   <StrictMode>
     <App/>
   </StrictMode>
